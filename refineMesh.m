5b4c106f4e1bb015c0746577d0359038d48fcf41
Vincent Magnoux <vincent.magnoux@polymtl.ca>
2016-12-16 20:35:05 -0500

    - New function to refine elements of a mesh

---
+++ refineMesh.m
@@ -0,0 +1,30 @@
+function [outNumNodes, outNumElem, outNodeCoords, outConnect] = refineMesh(nNodes, nodeCoords, connect, elementIds)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Refine the input mesh by dividing the chosen elements into 2 elements each
+%
+% Takes the initial number of nodes, their coordinates, the connectivity and
+% an array of elements to divide
+%
+% Returns the final number of nodes
+%
+
+outNumNodes = nNodes;
+outNodeCoords = nodeCoords;
+outConnect = connect;
+
+for iToDivide = 1:numel(elementIds)
+  iElem = elementIds(iToDivide);
+  elemMiddle = (nodeCoords(connect(iElem,1)) + nodeCoords(connect(iElem,2))) / 2;
+  outNumNodes = outNumNodes + 1;
+  outNodeCoords(outNumNodes) = elemMiddle;
+  outConnect(outNumNodes-1,2) = connect(iElem,2);
+  outConnect(iElem,2) = outNumNodes;
+  outConnect(outNumNodes-1,1) = outNumNodes;
+  
+  outNodeCoords;
+  outConnect;
+endfor
+
+outNumElem = outNumNodes - 1;
+
+endfunction
